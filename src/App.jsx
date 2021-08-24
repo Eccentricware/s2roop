aca9c872804fda92e11cbd66040606d52c2a3fa5
Joseph Peck <joe.r.peck@gmail.com>
2021-08-24 13:31:26 -0700

    extendSequence added

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,29 @@ const App = (props) => {
   const [height, setHeight] = useState(500);
   const [btnBorder, setBtnBorder] = useState(5);
 
+  const [interactionMode, setInteractionMode] = useState('awaitStart')
+  const [currentSequence, setCurrentSequence] = useState([]);
+
+  var activeOptions = ['red', 'blue', 'yellow', 'green'];
+
+  const extendSequence = () => {
+    var nextColor = Math.floor(Math.random() * activeOptions.length);
+    var extendedSequence = currentSequence.slice();
+    extendedSequence.push(activeOptions[nextColor]);
+    setCurrentSequence(extendedSequence);
+    console.log(extendedSequence);
+  }
+
   return (
     <div className="App">
       <CenterButtons width={width} height={height} />
+      <svg className="start-btn" width="100" height="50"
+        viewBox="0 0 100 50"
+        border="solid white 2px"
+        onClick={() => {extendSequence()}}
+      >
+        <text x="5" y="20">Start Game</text>
+      </svg>
     </div>
   )
 }
