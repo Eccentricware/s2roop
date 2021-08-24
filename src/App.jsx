1dc2f7924913d00e4f393ba3268d7c8d6a1dda1d
Joseph Peck <joe.r.peck@gmail.com>
2021-08-24 10:34:16 -0700

    React is initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+//import "./App.css";
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <h1> Hello, World! </h1>
+    </div>
+  )
+}
+
+export default App;
